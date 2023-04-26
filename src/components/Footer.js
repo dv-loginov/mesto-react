f1b22315af8bcbae18801406901455c04e5ce996
dv-loginov <dilog@rambler.ru>
2023-04-26 10:46:58 +0300

    deploy

--- src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-  return (
-    <footer className="footer">
-      <p className="footer__copyright">&#169; 2020 Mesto Russia</p>
-    </footer>
-  );
-}
-
-export default Footer;
--- src/components/Header.js
+++
@@ -1,9 +0,0 @@
-const Header = () => {
-  return (
-    <header className="header">
-      <a href="/" className="logo"> </a>
-    </header>
-  );
-}
-
-export default Header;
